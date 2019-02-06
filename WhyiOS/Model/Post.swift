6dd29dd87f0d4090746bed48bec0b5e15a18eba5
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-06 13:51:57 -0700

    Add Model

---
+++ WhyiOS/Model/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  WhyiOS
+//
+//  Created by Dominic Lanzillotta on 2/6/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import Foundation
+
+class Post: Codable {
+    let name: String
+    let cohort: String
+    let reason: String
+}
